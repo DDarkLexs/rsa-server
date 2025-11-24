4f451f86b4e671d272eb953bebc0a753dc6b72cb
lugogo <antoniolexis2001@gmail.com>
2025-11-25 00:13:36 +0100

    refactor: retified doenv file for rsa key

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,5 +3,5 @@ import * as path from "path";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-export const privateKey = String(process.env.PRIVATE_KEY).replace(/\\n/g, "\n");
-export const publicKey = String(process.env.PUBLIC_KEY).replace(/\\n/g, "\n");
+export const privateKey = String(process.env.RSA_PRIVATE_KEY).replace(/\\n/g, "\n");
+export const publicKey = String(process.env.RSA_PUBLIC_KEY).replace(/\\n/g, "\n");
